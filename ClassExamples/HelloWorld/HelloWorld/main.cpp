0060277df7e99780e73d3238d3dfbc9c02cf82e8
erictarrbelmont <erictarrbelmont@gmail.com>
2020-01-10 09:34:51 -0600

    Class Examples 1/9

---
+++ ClassExamples/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Eric Tarr on 1/9/20.
+//  Copyright © 2020 Eric Tarr. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // insert code here...
+    
+    cout << "Hello, World!" << endl;
+    
+    // Data type (int, float, double, long, bool)
+    int a = 5; // declared and defined in one command
+    
+    int b; // declare
+    b = 6; // define
+    
+    cout << a << " " << b << endl;
+    
+    float c;
+    c = 3.14;
+    
+    double d = -5.67;
+    
+    float x[5] = {0.0}; // initialize all to be zero
+    x[0] = 6;
+    x[1] = 3;
+    x[2] = 7;
+    
+    
+    return 0;
+}
+
+
+
