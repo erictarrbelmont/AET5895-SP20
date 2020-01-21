a835c0faae2e5341a1da999d565bb6b650664993
erictarrbelmont <erictarrbelmont@gmail.com>
2020-01-21 12:43:14 -0600

    Beginning of class 1/21

--- ClassExamples/AudioExample/AudioExample/main.cpp
+++ ClassExamples/AudioExample/AudioExample/main.cpp
@@ -7,8 +7,15 @@
 //
 
 #include <iostream>
+#include <cmath>
 #include "AudioFile/AudioFile.hpp"
 using namespace std;
+
+
+void dBAmpChange(vector<float> & x, const float dBAmp);
+
+void stereoWidener(vector<vector<float>> & x, const float width);
+
 
 int main() {
     
@@ -18,14 +25,20 @@ int main() {
     int numChannels;
     vector<float> signal;
     
-    audioread(filename,signal,Fs,bitDepth,numChannels);
+    //audioread(filename,signal,Fs,bitDepth,numChannels);
     
-    int N = signal.size(); // length(signal)
+//    int N = signal.size(); // length(signal)
+//
+//    for (int n = 0; n < N ; n++){
+//        signal[n] *= 0.25; // signal[n] = signal[n] * 0.25;
+//    }
     
-    for (int n = 0; n < N ; n++){
-        signal[n] *= 0.25; // signal[n] = signal[n] * 0.25;
-    }
+//    float dBAmp = -24.0f;
+//    dBAmpChange(signal, dBAmp);
     
+    //string outName = "Test.wav";
+    
+    //audiowrite(outName,signal,Fs,bitDepth,numChannels);
     
     //float stereoSignal[2][4] = {0.0f};
     //stereoSignal[1][3] = 5.0f;
@@ -34,18 +47,53 @@ int main() {
     vector<vector<float>> stereoSignal;
     audioread(stereoFilename,stereoSignal,Fs,bitDepth,numChannels);
     
-    N = stereoSignal[0].size();
-    
-    // Process each sample in both the left and right channels
-    for (int c = 0; c < numChannels ; c++){
-        for (int n = 0; n < N ; n++){
-            stereoSignal[c][n] *= 0.25f;
-        }
-    }
-    
+    float width = 0.0f; // 0 - 2
+    stereoWidener(stereoSignal, width);
+
+//
     string outName = "Test.wav";
     audiowrite(outName,stereoSignal,Fs,bitDepth,numChannels);
     
     
     return 0;
 }
+
+void dBAmpChange(vector<float> & x, const float dBAmp){
+    
+    float linAmp = pow(10.0f , (dBAmp/20.0f) );
+    
+    int N = x.size();
+    for (int n = 0; n < N ; n++){
+        x[n] *= linAmp;
+    }
+    
+}
+
+
+void stereoWidener(vector<vector<float>> & x, const float width){
+    
+    vector<float> sides;
+    vector<float> mids;
+    
+    int N = x[0].size();
+    
+    sides.resize(N);
+    mids.resize(N);
+    
+    for (int n = 0; n < N; n++){
+        sides[n] = 0.5f*(x[0][n] - x[1][n]);
+        mids[n] = 0.5f*(x[0][n] + x[1][n]);
+        
+        sides[n] *= width;
+        mids[n] *= (2.0f - width);
+        
+        x[0][n] = sides[n] + mids[n];
+        x[1][n] = sides[n] - mids[n];
+        
+    }
+    
+    
+    
+    
+}
+
--- ClassExamples/FunctionExamples/FunctionExamples/main.cpp
+++ ClassExamples/FunctionExamples/FunctionExamples/main.cpp
@@ -7,9 +7,49 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+float myFunction1(float x); // declaration
+
+void myFunction2(float &x, const float y); // function passing in a reference
+
+
+int main() {
+    
+    float a = 2.0f;
+    float b;
+    
+    const float c = 11.0f;
+    //c = 8;
+    
+    b = myFunction1(a);
+    
+    // Reference in C++
+    //float * c = &b;
+    
+    //cout << "Value of b is: " << b << endl;
+    //cout << "Reference of b is: " << c << endl;
+    
+    myFunction2(a,c);
+    
     return 0;
+}
+
+
+float myFunction1(float x){
+    // Passing value to function
+    // Will not overwrite value in "main"
+    float y = 10.f;
+    
+    x = y;
+    
+    return 1.0f;
+    
+}
+
+
+void myFunction2(float &x, const float y){
+    
+    x = 10.0f + y;
+    // no return necessary
 }
