c4808ae9b81c41b61838823dae8c15c067e2b2ee
erictarrbelmont <erictarrbelmont@gmail.com>
2020-01-21 12:23:39 -0600

    Progress during 1/16 lecture

--- ClassExamples/AudioExample/AudioExample/main.cpp
+++ ClassExamples/AudioExample/AudioExample/main.cpp
@@ -20,10 +20,31 @@ int main() {
     
     audioread(filename,signal,Fs,bitDepth,numChannels);
     
-    int N = signal.size();
+    int N = signal.size(); // length(signal)
+    
+    for (int n = 0; n < N ; n++){
+        signal[n] *= 0.25; // signal[n] = signal[n] * 0.25;
+    }
+    
+    
+    //float stereoSignal[2][4] = {0.0f};
+    //stereoSignal[1][3] = 5.0f;
+    
+    string stereoFilename = "stereoDrums.wav";
+    vector<vector<float>> stereoSignal;
+    audioread(stereoFilename,stereoSignal,Fs,bitDepth,numChannels);
+    
+    N = stereoSignal[0].size();
+    
+    // Process each sample in both the left and right channels
+    for (int c = 0; c < numChannels ; c++){
+        for (int n = 0; n < N ; n++){
+            stereoSignal[c][n] *= 0.25f;
+        }
+    }
     
     string outName = "Test.wav";
-    audiowrite(outName,signal,Fs,bitDepth,numChannels);
+    audiowrite(outName,stereoSignal,Fs,bitDepth,numChannels);
     
     
     return 0;
